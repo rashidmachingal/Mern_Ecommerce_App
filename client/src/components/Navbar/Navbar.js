b22eae4c5f24643cbf80fb87584a6f8c92dd77b4
Rashid Machingal <rashileocontact@gmail.com>
2022-07-05 18:30:21 +0530

    Responsive Navbar created

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import { Badge } from "@material-ui/core";
-import { ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge,IconButton,Tooltip } from "@material-ui/core";
+import {ShoppingCartOutlined,FavoriteBorderOutlined,PersonOutlineOutlined, Search, Menu, Close } from "@material-ui/icons";
 
 const Navbar = () => {
+
+  const cursorPointer = {cursor: 'pointer',color:"black"}
+  const [open, setOpen] = useState(false)
+
   return (
     <>
-      <Badge badgeContent={3} color="secondary">
-          <ShoppingCartOutlined />
-      </Badge>
+      <nav className="nav-container">
+        <div className="nav-menu">
+        <IconButton>{open? <Close onClick={()=>setOpen(!open)} style={cursorPointer} /> : <Menu onClick={()=>setOpen(!open)} style={cursorPointer} />}</IconButton>
+        </div>
+        <div className="nav-logo">
+          <img src="https://i.ibb.co/BZXZtfB/Anybuy-Case-Study-on-Behance-removebg-preview.png" alt="logo" />
+          <h2>RashCart</h2>
+        </div>
+        <div className="nav-items">
+          <span>Home</span>
+          <span>Shop</span>
+          <span>About</span>
+          <span>Contact</span>
+        </div>
+        <div className="nav-icons">
+          <div className="nav-search" ><IconButton><Search style={cursorPointer} /></IconButton></div>
+          <Tooltip title="Account"><IconButton><PersonOutlineOutlined style={cursorPointer} /></IconButton></Tooltip>
+          <div className="nav-whishlist" ><Tooltip title="Whishlist"><IconButton><FavoriteBorderOutlined style={cursorPointer} /></IconButton></Tooltip></div>
+          <Tooltip title="Cart"><IconButton><Badge badgeContent={3} color="primary" style={cursorPointer} ><ShoppingCartOutlined /></Badge></IconButton></Tooltip>
+        </div>
+        <div className="nav-icons-ml">
+            <Search/>
+            <PersonOutlineOutlined/>
+            <Badge badgeContent={3}  color="primary" ><ShoppingCartOutlined/></Badge>
+        </div>
+        <div className="nav-ml-list" style={{left:open ? "0px" : "-100vw"}} >
+         <span>Home</span>
+         <span>Orders</span>
+         <span>Shop</span>
+         <span>About</span>
+         <span>Contact</span>
+        </div>
+      </nav>
     </>
   );
 };
