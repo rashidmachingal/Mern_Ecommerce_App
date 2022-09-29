c0d97c18bf9c3bffefb06ed61e3765cc6d4ca67e
Rashid Machingal <rashileocontact@gmail.com>
2022-09-29 21:41:24 +0530

    top selling products slider added (homeProducts)

---
+++ client/src/components/HomeProducts/Data.js
@@ -0,0 +1,65 @@
+export const HomeProductsData = [
+    {
+        id:1,
+        brand:"Roadster",
+        title:"Grey Melange Solid Round Neck T-shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/3314157/2018/4/18/11524041081440-Roadster-Men-Grey-Melange-Solid-Round-Neck-T-shirt-3461524041081257-1.jpg",
+        price:559
+    },
+    {
+        id:2,
+        brand:"Urbano Fashion",
+        title:"Teal Green Slim Fit Tropical Printed Pure Cotton T-shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12377258/2020/9/11/ce1b7bcb-a65a-4eb0-a317-42ac02718f1e1599798741705UrbanoFashionPrintedMenRoundNeckDarkGreenT-Shirt1.jpg",
+        price:799
+    },
+    {
+        id:3,
+        brand:"The Indian Garage",
+        title:"White & Teal Blue Slim Fit Striped Casual Shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10673544/2019/9/24/6b9c7688-7ca2-4d11-9e5b-a3745ecd8f761569310358973-The-Indian-Garage-Co-Men-Shirts-8481569310357131-1.jpg",
+        price:1299
+    },
+    {
+        id:4,
+        brand:"Louis Philippe",
+        title:"Off White Slim Fit Formal Shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/19092316/2022/8/2/0cbc232d-46f3-4af8-96a3-e00936e3819e1659437072782-Louis-Philippe-Men-Shirts-8681659437072336-1.jpg",
+        price:1059
+    },
+    {
+        id:5,
+        brand:"Roadster",
+        title:"Olive Green Pure Cotton Sustainable Casual Shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11904108/2020/7/14/869203dc-200a-4284-9d2d-a06fd5ea582d1594707585600-Roadster-Men-Shirts-1041594707583386-1.jpg",
+        price:1399
+    },
+    {
+        id:6,
+        brand:"Roadster",
+        title:"Grey Melange Solid Round Neck T-shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/3314157/2018/4/18/11524041081440-Roadster-Men-Grey-Melange-Solid-Round-Neck-T-shirt-3461524041081257-1.jpg",
+        price:559
+    },
+    {
+        id:7,
+        brand:"The Indian Garage",
+        title:"White & Teal Blue Slim Fit Striped Casual Shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10673544/2019/9/24/6b9c7688-7ca2-4d11-9e5b-a3745ecd8f761569310358973-The-Indian-Garage-Co-Men-Shirts-8481569310357131-1.jpg",
+        price:1299
+    },
+    {
+        id:8,
+        brand:"Louis Philippe",
+        title:"Off White Slim Fit Formal Shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/19092316/2022/8/2/0cbc232d-46f3-4af8-96a3-e00936e3819e1659437072782-Louis-Philippe-Men-Shirts-8681659437072336-1.jpg",
+        price:1059
+    },
+    {
+        id:9,
+        brand:"Roadster",
+        title:"Grey Melange Solid Round Neck T-shirt",
+        image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/3314157/2018/4/18/11524041081440-Roadster-Men-Grey-Melange-Solid-Round-Neck-T-shirt-3461524041081257-1.jpg",
+        price:559
+    },
+]
--- client/src/components/HomeProducts/HomeProducts.js
+++ client/src/components/HomeProducts/HomeProducts.js
@@ -1,8 +1,75 @@
+import './HomeProducts.css'
+import { HomeProductsData } from './Data'
+import {ArrowBackIos, ArrowForwardIos} from '@mui/icons-material';
+import { useEffect, useRef, useState } from 'react';
 
 const HomeProducts = () => {
+
+  const [scrollX, setscrollX] = useState(0); // For detecting start scroll postion
+  const [scrolEnd, setscrolEnd] = useState(false); // For detecting end of scrolling
+  const scrl = useRef()
+
+  useEffect(() => {
+    //Check width of the scollings
+    if (
+      scrl.current &&
+      scrl?.current?.scrollWidth === scrl?.current?.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+    return () => {};
+  }, [scrl?.current?.scrollWidth, scrl?.current?.offsetWidth]);
+
+   //This will check scroll event and checks for scroll end
+  const scrollCheck = () => {
+    setscrollX(scrl.current.scrollLeft);
+    if (
+      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+      scrl.current.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  };
+
+  const handleSlide = (shift) => {
+    scrl.current.scrollLeft += shift;
+  };
+
   return (
     <>
-
+     <div className="homeProContainer">
+      <div className="homepro-title">
+        <h2>Top Selling Products</h2>
+      </div>
+      <div className="homepro-desc">
+        <span>
+         These are some of the most popular and top-selling items on Rash
+         Cart that you can buy right now.
+        </span>
+      </div>
+      <div onScroll={scrollCheck} ref={scrl} className="home-wrapper">
+        {HomeProductsData.map((i)=>{
+          return(
+            <div key={i.id} className="homepro-box">
+             <img src={i.image} alt="img" />
+             <div className="homepro-titles">
+              <h3>{i.brand}</h3>
+              <h4>{i.title}</h4>
+             </div>
+             <div className="homepro-price">
+              <h3>₹{i.price}</h3>
+             </div>
+            </div>
+          )
+        })}
+      </div>
+      { scrollX !== 0 && <div onClick={()=>handleSlide(-820)} style={{left:"70px"}}  className="homepro-arrow"><ArrowBackIos style={{marginLeft:"7px"}} /></div>}
+      { !scrolEnd && <div onClick={()=>handleSlide(+820)} style={{right:"70px"}} className="homepro-arrow"><ArrowForwardIos/></div>}
+     </div>
     </>
   )
 }
