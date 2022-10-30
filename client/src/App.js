de4a6e32c228d8ef1934392f297b82fa4ed65eed
Rashid Machingal <rashileocontact@gmail.com>
2022-10-30 20:52:46 +0530

    react router implemented category view page created

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Footer, Navbar} from './components';
 import HomePage from './pages/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CategoryViewPage from './pages/CategoryViewPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Routes>
        <Route path="/" element={<HomePage/>} />
+       <Route path="/category/:category" element={<CategoryViewPage/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
---
+++ client/src/pages/CategoryViewPage.js
@@ -0,0 +1,11 @@
+import {CategoryView} from '../components'
+
+const CategoryViewPage = () => {
+  return (
+    <>
+    <CategoryView/>
+    </>
+  )
+}
+
+export default CategoryViewPage
