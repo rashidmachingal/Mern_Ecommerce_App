d30b13cc824dc8ab9bc5139c1ca3a85aa3506655
Rashid Machingal <rashileocontact@gmail.com>
2023-01-21 12:18:29 +0530

    get all products api pagination added

--- server/routes/products.js
+++ server/routes/products.js
@@ -15,8 +15,10 @@ router.post("/create", async (req, res) => {
 
 // get all product
 router.get("/get-all", async (req, res) => {
+    const pageIndex = req.query.page
+    const limit = 5
     try {
-        const allProducts = await Product.find()
+        const allProducts = await Product.find().limit(limit).skip((pageIndex - 1) * limit)
         res.status(200).json(allProducts)
     } catch (error) {
         res.status(500).json(error);
