ad6568e0726888acbd2da1843ba9392527f19cd9
Rashid Machingal <rashileocontact@gmail.com>
2023-01-28 13:13:17 +0530

    set user info in localStorage

--- client/src/redux/user.js
+++ client/src/redux/user.js
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const user = JSON.parse(localStorage.getItem("user")) || ""
 const INITIAL_STATE = {
-    user_name : null,
-    userId : null,
-    token : null
+    user_name : user.user_name || null,
+    userId : user.userId || null,
+    token : user.token || null
 }
 
 const userSlice = createSlice({
@@ -14,6 +15,12 @@ const userSlice = createSlice({
             state.user_name = action.payload.user_name
             state.userId = action.payload.userId
             state.token = action.payload.token
+
+            localStorage.setItem("user", JSON.stringify({
+                user_name: state.user_name,
+                userId: state.userId,
+                token: state.token
+            }));
         }
     }
 })
